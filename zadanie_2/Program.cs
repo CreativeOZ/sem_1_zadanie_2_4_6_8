b572b43d26d2bcadfb841b7b7f5cd2269f3a8dfe
CreativeOZ <zagriaoleg@gmail.com>
2022-02-14 19:04:37 +0300

    Даны два числа. Показать большее и меньшее число.

---
+++ zadanie_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Даны два числа. Показать большее и меньшее число.
+Console.Clear();
+
+int a = 3;
+int b = 4;
+if(a > b)
+{
+Console.WriteLine(a + " больше чем, " + b);
+}   
+else
+{
+Console.WriteLine(b + " больше чем, " + a);
+}
