4792467b970d81e03d92f9a0a40aff838931134d
CreativeOZ <zagriaoleg@gmail.com>
2022-02-14 19:49:09 +0300

    Показать четные числа от 1 до N.

--- zadanie_8/Program.cs
+++ zadanie_8/Program.cs
@@ -3,12 +3,12 @@ Console.Clear();
 
 Console.Write("Введите число: ");
 int N = int.Parse(Console.ReadLine());
-int N2 = 1;
-int count = N;
-N = N - (N2*2);
-while (N <= count)
-{
-if (count % 2 == 0)
-{
-    Console.WriteLine(count + " ");
-}}
+int i = 1;
+  while(i<N) 
+  { 
+    i++;        
+   if(i%2 == 0) {
+    Console.Write("{0} ", i);
+   }  
+  }
+  Console.ReadKey();
