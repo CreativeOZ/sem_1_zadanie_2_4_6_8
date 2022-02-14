4e60f13aebc5965cae322125425f8546a0d8396b
CreativeOZ <zagriaoleg@gmail.com>
2022-02-14 19:37:58 +0300

    Найти максимальное из трех чисел.

---
+++ zadanie_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти максимальное из трех чисел
+Console.Clear();
+
+int NumberA = 11;
+int NumberB = 102;
+int NumberC = 1010;
+
+if(NumberA > NumberB && NumberA > NumberC)
+{
+Console.WriteLine("Nubmer Max = " + NumberA);
+}  
+if(NumberB > NumberC)
+{
+Console.WriteLine("Number Max = " + NumberB);
+}  
+else
+{
+Console.WriteLine("Number Max = " + NumberC);
+}
+Console.ReadLine();
