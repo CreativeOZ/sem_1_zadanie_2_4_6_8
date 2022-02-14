5f2df8748914ad16f915ec43fdbf2c7832eff60d
CreativeOZ <zagriaoleg@gmail.com>
2022-02-14 19:47:32 +0300

    Показать четные числа от 1 до N.

---
+++ zadanie_8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать четные числа от 1 до N
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int N2 = 1;
+int count = N;
+N = N - (N2*2);
+while (N <= count)
+{
+if (count % 2 == 0)
+{
+    Console.WriteLine(count + " ");
+}}
