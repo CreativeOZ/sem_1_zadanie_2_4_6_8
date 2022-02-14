967447b638f554b75a15b669aa9c2e8de89a9f4b
CreativeOZ <zagriaoleg@gmail.com>
2022-02-14 19:43:58 +0300

    Выяснить является ли число чётным.

---
+++ zadanie_6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить является ли число чётным
+Console.Clear();
+
+Console.Write("Введите целое число: ");
+int x = int.Parse(Console.ReadLine());
+int x2 = x % 2;
+Console.WriteLine(x2);
+if (x2 == 0)
+{
+Console.WriteLine("число четное");
+}
+else
+{
+Console.WriteLine("Число нечетное");
+Console.ReadLine();
+}
